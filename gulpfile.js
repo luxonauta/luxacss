1a14aff46ba624e69d2490fd98292746f58a6564
luxonauta <luxonauta@gmail.com>
2020-11-17 11:41:19 -0300

    Add navbar and change from scss to sass! :rocket:

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ const sass = require("gulp-sass");
 sass.compiler = require("node-sass");
 
 exports.default = () => {
-    gulp.watch("sass/*/*.scss", compileSass);
+    gulp.watch("sass/*/*.sass", compileSass);
 }
 
 const compileSass = () => {
-    return gulp.src("sass/luxa.scss")
+    return gulp.src("sass/luxa.sass")
         .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
         .pipe(gulp.dest("dist/expanded"))
         .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
