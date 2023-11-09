f73856d2994d7096a092def07a3867b0df69fc28
Lucas de França <luxon4uta@gmail.com>
2023-11-09 18:36:30 -0300

    🗺️ Add Sourcemaps for Sass files

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,24 +3,41 @@ import gulp from "gulp";
 import postcss from "gulp-postcss";
 import sassEmbedded from "sass-embedded";
 import sassModule from "gulp-sass";
+import sourcemaps from "gulp-sourcemaps";
 
 const sass = sassModule(sassEmbedded);
 
-const createSassTask = (outputStyle) => () => {
-  return gulp
-    .src("./sass/luxa.scss")
-    .pipe(sass({ outputStyle }).on("error", sass.logError))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(gulp.dest(`dist/${outputStyle}`));
+const SASS_SRC = "./sass/luxa.scss";
+const SASS_DEST = "dist";
+const SASS_MAPS = "./maps";
+
+const sassErrorHandler = (error) => {
+  sass.logError(error);
+  this.emit("end");
 };
 
-const compileSassExpanded = createSassTask("expanded");
-const compileSassCompressed = createSassTask("compressed");
+const compileSass = (outputStyle) => {
+  return gulp
+    .src(SASS_SRC)
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle }).on("error", sassErrorHandler))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write(SASS_MAPS))
+    .pipe(gulp.dest(`${SASS_DEST}/${outputStyle}`));
+};
 
-const compileSass = gulp.parallel(compileSassExpanded, compileSassCompressed);
+const compileSassExpanded = () => compileSass("expanded");
+const compileSassCompressed = () => compileSass("compressed");
 
 const watchTask = () => {
-  gulp.watch("./sass/**/*.scss", gulp.series(compileSass));
+  gulp.watch(
+    "./sass/**/*.scss",
+    gulp.series(compileSassExpanded, compileSassCompressed),
+  );
 };
 
-export default gulp.series(compileSass, watchTask);
+export default gulp.series(
+  compileSassExpanded,
+  compileSassCompressed,
+  watchTask,
+);
