25c2da9717dfe4130b5e2484b67257feab9b36fa
Lucas de França <luxon4uta@gmail.com>
2024-11-05 19:00:27 -0300

    🎈 v2.0.2

---
+++ docs/src/components/illustrations/columns/index.tsx
@@ -0,0 +1,17 @@
+import "./index.scss";
+
+export const Columns = () => (
+  <div className="illustration columns">
+    <code>row</code>
+    <div className="row">
+      <div className="col is-4">
+        <code>col</code>
+        <span>Column</span>
+      </div>
+      <div className="col">
+        <code>col</code>
+        <span>Column</span>
+      </div>
+    </div>
+  </div>
+);
