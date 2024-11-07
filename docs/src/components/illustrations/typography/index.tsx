fa64dadbf935d8a17ec6dbddcae4aecaeaec0d84
Lucas de França <luxon4uta@gmail.com>
2024-11-07 01:34:34 -0300

    🍞 Avoid DRY

--- docs/src/components/illustrations/typography/index.tsx
+++ docs/src/components/illustrations/typography/index.tsx
@@ -1,32 +1,23 @@
 import "./index.scss";
 
+const textClasses = [
+  { className: "text-01", code: "--text-01" },
+  { className: "text-02", code: "--text-02" },
+  { className: "text-03", code: "--text-03" },
+  { className: "text-04", code: "--text-04" },
+  { className: "text-05", code: "--text-05" },
+  { className: "text-06", code: "--text-06" }
+];
+
 export const Typography = () => (
   <div className="illustration typography">
     <div className="row">
-      <div className="info text-01">
-        <code>--text-01</code>
-        <span>In a hole in the ground there lived a hobbit.</span>
-      </div>
-      <div className="info text-02">
-        <code>--text-02</code>
-        <span>In a hole in the ground there lived a hobbit.</span>
-      </div>
-      <div className="info text-03">
-        <code>--text-03</code>
-        <span>In a hole in the ground there lived a hobbit.</span>
-      </div>
-      <div className="info text-04">
-        <code>--text-04</code>
-        <span>In a hole in the ground there lived a hobbit.</span>
-      </div>
-      <div className="info text-05">
-        <code>--text-05</code>
-        <span>In a hole in the ground there lived a hobbit.</span>
-      </div>
-      <div className="info text-06">
-        <code>--text-06</code>
-        <span>In a hole in the ground there lived a hobbit.</span>
-      </div>
+      {textClasses.map(({ className, code }) => (
+        <div key={className} className={`info ${className}`}>
+          <code>{code}</code>
+          <span>In a hole in the ground there lived a hobbit.</span>
+        </div>
+      ))}
     </div>
   </div>
 );
