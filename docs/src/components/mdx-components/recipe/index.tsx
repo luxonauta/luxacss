88f660555284f6f7afb2db1c516db1c1391bd3f6
Lucas de França <luxon4uta@gmail.com>
2024-12-02 23:18:38 -0300

    🧪 Test with absolute `baseDir`

--- docs/src/components/mdx-components/recipe/index.tsx
+++ docs/src/components/mdx-components/recipe/index.tsx
@@ -1,5 +1,5 @@
 import "./index.scss";
-import type { ReactElement } from "react";
+import { type ReactElement, use } from "react";
 import { Tabs } from "@/components/tabs";
 import { getBasePath } from "@/lib/environment";
 import CodeBlock from "../code-block";
@@ -21,8 +21,8 @@ const fetchSourceCode = async (dirName: string) => {
   }
 };
 
-export const Recipe = async ({ component, dirName }: RecipeProps) => {
-  const sourceCode = await fetchSourceCode(dirName);
+export const Recipe = ({ component, dirName }: RecipeProps) => {
+  const sourceCode = use(fetchSourceCode(dirName));
 
   if (!sourceCode)
     return <span>Something went wrong. Please try again later.</span>;
